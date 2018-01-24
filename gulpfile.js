a10b615c690f006d3963b01878131c830431f1c2
stefanpejovskisymphony <stefan.pejovski@symphony-solutions.eu>
2018-01-24 14:39:46 +0100

    Concatenated JavaScript Files

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,21 @@
 var gulp = require('gulp'),
-	gutil = require('gulp-util');
+	gutil = require('gulp-util'),
+	concat = require('gulp-concat');
+
+
+
+var jsSources = [
+	'components/scripts/calculation.js',
+	'components/scripts/writing-data.js'
+];
+
 
 gulp.task('log', function(){
 	gutil.log("Testing gulp");
+});
+
+gulp.task('js', function(){
+	gulp.src(jsSources)
+		.pipe(concat('script.js'))
+		.pipe(gulp.dest('builds/development/js'))
 });
